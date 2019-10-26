25d3be12a743a7871b40cf13316aa8641243167f
PinuoC <104079381@qq.com>
2019-10-26 19:59:43 +0800

    init

---
+++ src/main/java/com/caohangwei/mallvueserver/MallVueServerApplication.java
@@ -0,0 +1,13 @@
+package com.caohangwei.mallvueserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallVueServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallVueServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/caohangwei/mallvueserver/MallVueServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caohangwei.mallvueserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallVueServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
